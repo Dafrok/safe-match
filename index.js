9cbfaa1a08bfe51287df5312001f6d2bab60e9d4
Dafrok <o.o@mug.dog>
2016-03-29 17:24:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@
     var match = String.prototype.match
     return {
         match: function (str, regExp) {
-            return str.match(regExp) || []
+            return str.match(regExp) || 0
         },
         init: function (options) {
             if (Object.prototype.toString.call(options) === '[object Object]') {
                 var name = options.name
                 var noConflict = options.noConflict
                 String.prototype[name || 'safeMatch'] = function (regExp, noConflict) {
-                    return noConflict ? (match.call(this, regExp) || []) : match.call(this, regExp)
+                    return noConflict ? (match.call(this, regExp) || 0) : match.call(this, regExp)
                 }
             } else {
                 String.prototype.safeMatch = function (regExp) {
-                    return match.call(this, regExp) || []
+                    return match.call(this, regExp) || 0
                 }
             }
         },
